21074b0cdbe185782572644a20a7abe7207803cf
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2022-12-12 09:07:16 -0300

    chore: move the event listener function to the parent component

--- src/components/Parent/index.tsx
+++ src/components/Parent/index.tsx
@@ -1,28 +1,19 @@
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import { Child } from '../Child';
 import styles from './styles.module.css';
 
 export function Parent() {
   const childRef = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
-    function handleChildClick(event: MouseEvent) {
-      if(!childRef.current?.contains(event.target as Node))
-        console.log('outside');
-      else
-        console.log('inside');
-    }
-
-    document.addEventListener('click', handleChildClick);
-
-    return () => {
-      document.removeEventListener('click', handleChildClick);
-    };
-  }, []);
+  function handleClick(target: EventTarget) {
+    if(!childRef.current?.contains(target as Node))
+      console.log('outside');
+    else
+      console.log('inside');
+  }
 
   return (
-    <div className={styles.parent}>
-      {/* <div ref={childRef} style={{ width: '100px', height: '100px', backgroundColor: 'blue' }} /> */}
+    <div className={styles.parent} onClick={(event) => handleClick(event.target)}>
       <Child ref={childRef} />
     </div>
   );
